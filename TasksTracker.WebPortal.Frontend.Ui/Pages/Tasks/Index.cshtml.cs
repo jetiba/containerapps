ddeeea6e48999dd7bf3a886c35bc5fecbaa1d855
Jessica Tibaldi <j.tibaldi@outlook.it>
2025-02-25 15:12:08 +0100

    Change frontend UI with Dapr Integration

--- TasksTracker.WebPortal.Frontend.Ui/Pages/Tasks/Index.cshtml.cs
+++ TasksTracker.WebPortal.Frontend.Ui/Pages/Tasks/Index.cshtml.cs
@@ -1,20 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using TasksTracker.WebPortal.Frontend.Ui.Pages.Tasks.Models;
+using Dapr.Client;
 
 namespace TasksTracker.WebPortal.Frontend.Ui.Pages.Tasks
 {
     public class IndexModel : PageModel
     {
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly DaprClient _daprClient;
         public List<TaskModel>? TasksList { get; set; }
 
         [BindProperty]
         public string? TasksCreatedBy { get; set; }
 
-        public IndexModel(IHttpClientFactory httpClientFactory)
+        public IndexModel(IHttpClientFactory httpClientFactory, DaprClient daprClient)
         {
             _httpClientFactory = httpClientFactory;
+            _daprClient = daprClient;    
         }
 
         public async Task<IActionResult> OnGetAsync()
@@ -22,9 +25,12 @@ namespace TasksTracker.WebPortal.Frontend.Ui.Pages.Tasks
             TasksCreatedBy = Request.Cookies["TasksCreatedByCookie"];
 
             if (!String.IsNullOrEmpty(TasksCreatedBy)) {
-                // direct svc to svc http request
-                var httpClient = _httpClientFactory.CreateClient("BackEndApiExternal");
-                TasksList = await httpClient.GetFromJsonAsync<List<TaskModel>>($"api/tasks?createdBy={TasksCreatedBy}");
+                // Invoke via internal URL (without Dapr)
+                // var httpClient = _httpClientFactory.CreateClient("BackEndApiExternal");
+                // TasksList = await httpClient.GetFromJsonAsync<List<TaskModel>>($"api/tasks?createdBy={TasksCreatedBy}");
+                
+                // Invoke via DaprSDK (invoke HTTP Service using Dapr)
+                TasksList = await _daprClient.InvokeMethodAsync<List<TaskModel>>(HttpMethod.Get, "tasksmanager-backend-api", $"api/tasks?createdBy={TasksCreatedBy}");
                 return Page();
             } else {
                 return RedirectToPage("../Index");
@@ -34,16 +40,22 @@ namespace TasksTracker.WebPortal.Frontend.Ui.Pages.Tasks
         public async Task<IActionResult> OnPostDeleteAsync(Guid id)
         {
             // direct svc to svc http request
-            var httpClient = _httpClientFactory.CreateClient("BackEndApiExternal");
-            var result = await httpClient.DeleteAsync($"api/tasks/{id}");
+            // var httpClient = _httpClientFactory.CreateClient("BackEndApiExternal");
+            // var result = await httpClient.DeleteAsync($"api/tasks/{id}");
+
+            // Invoke via DaprSDK (invoke HTTP Service using Dapr)
+            await _daprClient.InvokeMethodAsync(HttpMethod.Delete, "tasksmanager-backend-api", $"api/tasks/{id}");
             return RedirectToPage();
         }
 
         public async Task<IActionResult> OnPostCompleteAsync(Guid id)
         {
             // direct svc to svc http request
-            var httpClient = _httpClientFactory.CreateClient("BackEndApiExternal");
-            var result = await httpClient.PutAsync($"api/tasks/{id}/markcomplete", null);
+            // var httpClient = _httpClientFactory.CreateClient("BackEndApiExternal");
+            // var result = await httpClient.PutAsync($"api/tasks/{id}/markcomplete", null);
+
+            // Invoke via DaprSDK (invoke HTTP Service using Dapr)
+            await _daprClient.InvokeMethodAsync(HttpMethod.Put, "tasksmanager-backend-api", $"api/tasks/{id}/markcomplete");
             return RedirectToPage();
         }
     }
---
+++ scripts/3-config-dapr.sh
@@ -0,0 +1,8 @@
+# Install Dapr 
+wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
+
+# Initialize Dapr
+# Docker must be running on the machine where you run this command
+dapr init
+
+
